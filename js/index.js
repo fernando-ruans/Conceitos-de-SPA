47efd0df325b75d59dfc24c56744e389e40acd05
Fernando <fernando_ruans@hotmail.com>
2023-10-18 11:48:51 -0300

    Orientação a objetos no JavaScript.

--- js/index.js
+++ js/index.js
@@ -1,30 +1,13 @@
-const routes = {
-    "/": "/pages/home.html",
-     "/about": "/pages/about.html",
-    "/contact": "/pages/contact.html",
-    404: "/pages/404.html",
-    }
+import './router.js'
 
-    function route(event) {
-        event = event || window.event
-        event.preventDefault()
+//const routes = {
+//    "/": "/pages/home.html",
+//     "/about": "/pages/about.html",
+//   "/contact": "/pages/contact.html",
+//    404: "/pages/404.html",
+// }  
+    
+// handle()
 
-        window.history.pushState({}, "", event.target.href)
-
-        handle()
-    }
-        
-    function handle(){
-        const {pathname, href, host, port} = window.location
-        const route = routes[pathname] || routes[404]
-        fetch(route)
-        .then(data => data.text())
-        .then(html => {
-            document.querySelector('#app').innerHTML = html
-        })
-    }
-
-    handle()
-
-    window.onpopstate = () => handle()
-    window.route = () => route()
+// window.onpopstate = () => handle()
+// window.route = () => route()
